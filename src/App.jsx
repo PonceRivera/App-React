820915c30637d53bd3241440f0616e5adf9f6545
GitHub User <user@github.com>
2025-12-18 19:26:17 -0600

    snowfall

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
 import Home from './pages/Home'
+import Snowfall from 'react-snowfall'
 
 function App() {
   return (
     <>
+      <div style={{ position: 'fixed', inset: 0, zIndex: 10, pointerEvents: 'none' }}>
+        <Snowfall />
+      </div>
       <div className="safety-stripes-left"></div>
       <div className="safety-stripes-right"></div>
       <main>
